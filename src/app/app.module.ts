6a1c0b8b1a606ef2e1e1d9f027d937f11628ff36
Daniel <daniel.liemng@gmail.com>
2023-08-07 09:21:10 -0400

    simple counter - input, output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterOutputComponent } from './components/counter/counter-output/counter-output.component';
+import { CounterButtonsComponent } from './components/counter/counter-buttons/counter-buttons.component';
+import { CounterComponent } from './pages/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterOutputComponent,
+    CounterButtonsComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.scss'],
+})
+export class CounterButtonsComponent {
+  @Output() increment = new EventEmitter<void>();
+  @Output() decrement = new EventEmitter<void>();
+  @Output() reset = new EventEmitter<void>();
+
+  constructor() {}
+
+  onIncrement() {
+    this.increment.emit();
+  }
+
+  onDecrement() {
+    this.decrement.emit();
+  }
+
+  onReset() {
+    this.reset.emit();
+  }
+}
